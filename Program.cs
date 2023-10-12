7da7e26c6410d0f5d2b71effa2f8e6dd5019f9eb
Abhinav Kumar <kabhinav713@gmail.com>
2023-10-12 08:50:16 +0530

    [Abhinav]-Adding Count Of Remaining Chapters part

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Day11_Practice
+{
+    internal class Program
+    {
+        static int findremainingchapters(int N, List<(int, int)> list, int k)
+        {
+            int chaptersleft = 0;
+            foreach ((int startpage, int endpage) in list)
+            {
+                if (k >= startpage && k <= endpage)
+                {
+                    chaptersleft = N - list.IndexOf((startpage, endpage));
+                    break;
+                }
+            }
+            return chaptersleft;
+        }
+        static void Main(string[] args)
+        {
+            int N = 4, k = 7;
+            List<(int, int)> list = new List<(int, int)> { (1, 4), (5, 7), (9, 16), (17, 26) };
+            int chapters = findremainingchapters(N, list, k);
+            Console.WriteLine(chapters);
+        }
+    }
+}
